ae8a296c2c7f45a1d0bba0470b63b17ab91c4c59
Neagu Tudor <tudorneagu7@gmail.com>
2022-10-11 20:35:39 +0300

    made the details page

---
+++ details.js
@@ -0,0 +1,38 @@
+const showProductDetails = async () => {
+	const searchParamString = window.location.search;
+
+	const searchParams = new URLSearchParams(searchParamString);
+
+	const productId = searchParams.get('product_id');
+
+	
+	const productURL = `https://63372212132b46ee0bddc50f.mockapi.io/product/${productId}`;
+	const result = await fetch(productURL);
+	const productInfo = await result.json();
+
+	const productCardDetails = `
+      <div>
+	  <div class="card mb-3 mt-4 text-bg-dark product-det" >
+	  <div class="row g-0">
+		<div class="col-md-4">
+		  <img src="${productInfo.img}" class="img-fluid rounded-start " alt="productimg ">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title text-warning mb-3">${productInfo.name}</h5>
+			<p class="card-text">${productInfo.descr}.</p>
+			<h5 class="card-title text-warning mt-4">${productInfo.price}$</h5>
+			<button type="button" class="btn btn-outline-warning mt-2">Add to cart</button>
+
+			
+		  </div>
+		</div>
+	  </div>
+	</div>
+      </div>
+   `;
+
+	document.querySelector('.product-details').innerHTML = productCardDetails;
+};
+
+window.addEventListener('DOMContentLoaded', showProductDetails);
