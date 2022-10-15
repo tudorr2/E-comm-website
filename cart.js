995ee046b52c1811b1a25f9eb18252cbe6fa4fa0
Neagu Tudor <tudorneagu7@gmail.com>
2022-10-15 22:59:37 +0300

    added delete button and quantity input

--- cart.js
+++ cart.js
@@ -1,32 +1,39 @@
 const loadProducts = () => {
-	const productIds = JSON.parse(localStorage.getItem('products'));
+  
 
-	console.log(productIds);
+  const productIds = JSON.parse(localStorage.getItem("products"));
 
-	const products = [];
+  console.log(productIds);
 
-	const createCardFromProduct = (product) => {
-		return `<tr>
-        <th scope="row">${product.id}</th>
+  const products = [];
+
+  const createCardFromProduct = (product) => {
+      return `<tr>
+      <th scope="row"><input type = "number" class = "input-group-sm quantity"></th>
         <td><img src="${product.img}" class = "cart-img"></td>
         <td>${product.name}</td>
-        <td>${product.price}</td>
-        <td><button class = "btn btn-outline-warning"><i class="fa-solid fa-trash"></i></button></td>
+        <td>${product.price} $</td>
+        <td><button class = "btn btn-outline-warning delete-product"><i class="fa-solid fa-trash"></i></button></td>
 
       </tr>
   `;
-	};
+  };
 
-	productIds.forEach(async (productId) => {
-		const result = await fetch(
-			`https://63372212132b46ee0bddc50f.mockapi.io/product/${productId}`
-		);
+  productIds.forEach(async (productId) => {
+    const result = await fetch(
+      `https://63372212132b46ee0bddc50f.mockapi.io/product/${productId}`
+    );
 
-		const product = await result.json();
-		const innerHTMLProduct = createCardFromProduct(product);
+    const product = await result.json();
+    const innerHTMLProduct = createCardFromProduct(product);
 
-		document.querySelector('.tbody').innerHTML += innerHTMLProduct;
-	});
+    document.querySelector(".tbody").innerHTML += innerHTMLProduct;
+  });
 };
 
-window.addEventListener('DOMContentLoaded', loadProducts);
+window.addEventListener("DOMContentLoaded", loadProducts);
+
+const deleteBtn = document.querySelector(".delete-product");
+  deleteBtn.addEventListener("click", () => {
+    innerHTMLProduct.innerHTML = "";
+  });
