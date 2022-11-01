abdc4752587cfeb0f7e0cd7f110cd21d077174cf
Neagu Tudor <tudorneagu7@gmail.com>
2022-11-01 13:41:41 +0200

    ff

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const createCardFromProduct = (product) => {
 	  <a href="/src/html/details.html?product_id=${product.id}" class="card-title">
 	  <h5 class="card-title mb-2 text-light">${product.name}</h5></a>
 	  <h5 class="card-title mb-3">${product.price}$</h5>
-    <a href="/src/html/details.html?product_id=${product.id}" class="btn btn-dark btn-outline-warning btn-card">See details</a>
+    <a href="./src/html/details.html?product_id=${product.id}" class="btn btn-dark btn-outline-warning btn-card">See details</a>
 
 	</div>
   </div>`;
