c7f1270a6a1fc2003250e0e281a85d84d620df58
Neagu Tudor <tudorneagu7@gmail.com>
2022-11-05 19:32:36 +0200

    resolved some bugs i think

--- src/js/cart.js
+++ src/js/cart.js
@@ -16,30 +16,28 @@ let basket = JSON.parse(localStorage.getItem("basket")) || [];
 function removeItemFromCart(productId) {
   let temp = cart.filter((item) => item.id != productId);
   localStorage.setItem("cart", JSON.stringify(temp));
+  localStorage.setItem("basket", JSON.stringify(temp));
+  
   document.querySelector(".tbody").innerText = "";
   window.location.reload();
 }
 
-
-
 let increment = (id) => {
   let selectedItem = id;
-  
-  let search = basket.find((x) => x.id === selectedItem );
+
+  let search = basket.find((x) => x.id === selectedItem);
 
   if (search === undefined) {
     basket.push({
       id: selectedItem,
       item: 1,
-      
     });
   } else {
-    search.item += 1; 
+    search.item += 1;
   }
-
-  
-  localStorage.setItem("basket", JSON.stringify(basket));
   getTotal();
+
+  localStorage.setItem("basket", JSON.stringify(basket));
 
   // console.log(basket);
   update(selectedItem);
@@ -53,26 +51,27 @@ let decrement = (id) => {
     search.item -= 1;
   }
   localStorage.setItem("basket", JSON.stringify(basket));
-
+  
   // console.log(basket);
-
+  
   getTotal();
   update(selectedItem);
 };
 
-let update = (id , price) => {
-  let search = basket.find((x) => x.id === id );
+let update = (id, price) => {
+  let search = basket.find((x) => x.id === id);
 
   console.log("merge da ma dracu");
+  console.log(search.price);
   document.getElementById(id).innerHTML = search.item;
+
 };
 //make rows
 
 const loadCart = () => {
   let cart = JSON.parse(localStorage.getItem("cart"));
-  
+
   const createCardFromProduct = (product) => {
-    
     return `<tr>
     <th scope="row">${index}</th>
     <td><img src="${product.img}" class = "cart-img"></td>
@@ -96,8 +95,8 @@ const loadCart = () => {
   cart.forEach(async (productId) => {
     const result = await fetch(
       `https://63372212132b46ee0bddc50f.mockapi.io/product/${productId.id}`
-      );
-      const product = await result.json();
+    );
+    const product = await result.json();
     const innerHTMLProduct = createCardFromProduct(product);
     index++;
     document.querySelector(".tbody").innerHTML += innerHTMLProduct;
@@ -110,41 +109,38 @@ document.querySelector(".buy-btn").addEventListener("click", () => {
   alert("This is just a personal project👽");
 });
 
-
 // let TotalAmount = () => {
-  //   if(basket.length !== 0 ){
-    //     let amount = basket.map((x) =>{
-      //       let {item , id} = x;
-      //       let search = products.find((y) => y.id === id) || [];
-      
-      //       return item * search.price;
-      
-      //     })
-      //     console.log(amount);
-      //   }
-      //   else return;
-      // }
-      
-      // TotalAmount();
-      
-      function getTotal() {
-        let quantity = JSON.parse(localStorage.getItem("basket"));
-        let temp2 = quantity.map((basket) => {
-          return parseInt(basket.item)
-        });
-        let quantity2 = temp2.reduce((item) =>{
-          return item;
-        })
-        let temp = cart.map( (item) => {
-          return parseFloat(item.price);
-        } ,0);
-        let sum = temp.reduce(function (prev, next) {
-          return next + prev * quantity2 ;
-        }, 0);
-        
-        document.querySelector(
-          ".totalPrice"
-          ).innerHTML = `<h1>Total Price: <span class="text-warning">$${sum}</span></h1>`;
-        }
-        
- 
+//   if(basket.length !== 0 ){
+//     let amount = basket.map((x) =>{
+//       let {item , id} = x;
+//       let search = products.find((y) => y.id === id) || [];
+
+//       return item * search.price;
+
+//     })
+//     console.log(amount);
+//   }
+//   else return;
+// }
+
+// TotalAmount();
+
+function getTotal() {
+  let quantity = JSON.parse(localStorage.getItem("basket"));
+  let temp2 = quantity.map((basket) => {
+    return parseFloat(basket.item);
+  });
+  let quantity2 = temp2.reduce((prev, next) => {
+    return next + prev
+  });
+  let temp = cart.map((item) => {
+    return parseFloat(item.price);
+  }, 0);
+  let sum = temp.reduce(function (prev, next) {
+    return prev + next * temp2 ;
+  }, 0);
+
+  document.querySelector(
+    ".total-price"
+  ).innerHTML = `<h1 class="text-dark total-price">Total price: <span class="text-warning">$${sum}</span></span></h1> `;
+}
