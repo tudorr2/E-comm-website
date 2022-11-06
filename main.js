cc89e40e3e138d6b77f4e03e6f49c426e218b704
Neagu Tudor <tudorneagu7@gmail.com>
2022-11-06 21:08:54 +0200

    d

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 const createCardFromProduct = (product) => {
   return `<div class="card text-light mb-3 mt-3  product-card" style="width: 18rem;">
 	
-	<a href="/../src/html/details.html?product_id=${product.id}" class="card-img-top "><img src="${product.img}" class="card-img-top" alt="productimg"></a>
+	<a href="./details.html?product_id=${product.id}" class="card-img-top "><img src="${product.img}" class="card-img-top" alt="productimg"></a>
 	<div class="card-body">
-	  <a href="/../src/html/details.html?product_id=${product.id}" class="card-title a-main">
+	  <a href="./details.html?product_id=${product.id}" class="card-title a-main">
 	  <h5 class="card-title mb-2 text-light">${product.name}</h5></a>
 	  <h5 class="card-title mb-3">${product.price}$</h5>
     <a href="./details.html?product_id=${product.id}" class="btn btn-dark btn-outline-light btn-card btn-details">See details</a>
